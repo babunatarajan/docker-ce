c918323cb591fb88c7186d22d46ed9cf4244952b
Babu Natarajan <babunatarajan6@gmail.com>
2021-02-09 09:06:15 -0500

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+docker_compose_ver="1.28.2"
+
+apt-get -y update
+
+apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" -y
+
+curl -fsSL get.docker.com | sh
+
+curl -L "https://github.com/docker/compose/releases/download/$docker_compose_ver/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
