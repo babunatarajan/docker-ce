2528b6dbc8b7672ffa8134c75d98f1e4b8006356
Babu Natarajan <babunatarajan6@gmail.com>
2022-01-25 15:04:06 -0500

    Update install-docker-ubuntu.sh

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -16,10 +16,10 @@ apt-get -y install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable" -y
+#add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable" -y
 
 curl -fsSL get.docker.com | sh
 
